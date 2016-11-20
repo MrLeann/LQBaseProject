9a63fcea1879edadf5ff16300228f887bd2286a3
MrLeann <mrleann@foxmail.com>
2016-11-20 17:19:46 +0800

    WeiboToast

---
+++ LQBaseProject/Items/首页/类似微博提示View/Demo1/ReminderSceneOneVC.m
@@ -0,0 +1,41 @@
+//  ReminderSceneOneVC.m
+//  LQBaseProject
+//  Created by YZR on 16/11/20.
+//  Copyright © 2016年 YZR. All rights reserved.
+
+#import "ReminderSceneOneVC.h"
+#import "LeafNotification.h"
+
+
+@interface ReminderSceneOneVC ()
+
+@end
+
+@implementation ReminderSceneOneVC
+
+-(void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self mInitUI];
+    
+//    [self mInitData];
+}
+
+-(void)mInitUI{
+    
+}
+
+#pragma mark - 按钮一点击
+- (IBAction)mFirstButtonClick:(UIButton *)sender{
+    /**
+     *  传入了self，是添加到vc控制器上的，不是UIWindow哦。
+     */
+    [LeafNotification showInController:self withText:@"我是一个粉刷匠"];
+}
+
+#pragma mark - 按钮二点击
+- (IBAction)mSecondButtonClick:(UIButton *)sender{
+    [LeafNotification showInController:self withText:@"我是一个粉刷匠" type:LeafNotificationTypeSuccess];
+}
+
+@end
--- LQBaseProject/Items/首页/类似微博提示View/Host/ReminderTableVIew.m
+++ LQBaseProject/Items/首页/类似微博提示View/Host/ReminderTableVIew.m
@@ -1,40 +1,60 @@
-//
 //  ReminderTableVIew.m
 //  LQBaseProject
-//
 //  Created by YZR on 16/10/9.
 //  Copyright © 2016年 YZR. All rights reserved.
-//
 
 #import "ReminderTableVIew.h"
+#import "ReminderSceneOneVC.h"
 
-@interface ReminderTableVIew ()
+@interface ReminderTableVIew (){
+    NSArray *_titleArray;
+    
+}
 
 @end
 
 @implementation ReminderTableVIew
 
-- (void)viewDidLoad {
+-(void)viewDidLoad {
     [super viewDidLoad];
     
     
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
+    
+    self.navigationItem.title = @"类似微博提示框";
+    
+    _titleArray = @[@"1- 情形一",@"2- 情形二"];
+    
 }
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 5;
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     return 1;
 }
 
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return _titleArray.count;
+}
+
 #pragma mark - 返回cell
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
-    
-    
+    cell.textLabel.text = _titleArray[indexPath.row];
     return cell;
 }
+
+#pragma mark - 选中cell
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+
+    if(indexPath.row == 0){
+        
+        ReminderSceneOneVC *vc = [ReminderSceneOneVC new];
+        [self.navigationController pushViewController:vc animated:YES];
+    }else if(indexPath.row == 1){
+        
+    }
+}
+
+
 
 @end
